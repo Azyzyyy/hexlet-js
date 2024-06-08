4a4d47603f41053757ece0ad039c44ce6c93e176
Pinyaev Vladimir <avallakhh@gmail.com>
2024-06-08 15:59:59 +0300

    setting eslint and making corrections

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { outputondisplay } from "./src/index.js";
 import { retunlastval } from "./src/index.js";
-import _ from "lodash";
 
 console.log(outputondisplay());
 console.log(retunlastval(["one", "two"]));
